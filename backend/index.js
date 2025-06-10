a5ea734fbab80c936bec9d31c93a25104dae6e09
mahi13singh2004 <mahi13singh2004@gmail.com>
2025-06-11 00:40:52 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -7,5 +7,5 @@ dotenv.config()
 const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on the port ${PORT}`);
 })
